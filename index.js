7366efc9b692a3fa3d9cd9858e8e6547e46402ae
linkel <ironcarbide@gmail.com>
2019-02-11 13:07:41 -0800

    DELETE request is functional.

--- index.js
+++ index.js
@@ -55,4 +55,26 @@ server.post('/api/users', (req, res) => {
   }
 })
 
+server.delete('/api/users/:id', (req, res) => {
+  const id = req.params.id;
+  db.findById(id)
+  .then(user => {
+    if (user.length < 1) {
+      res.status(404).json({message: "The user with the specified ID does not exist."})
+    } else {
+      db.remove(id)
+      .then(response => {
+        res.status(200).json(user)
+      })
+      .catch(err => {
+        res.status(500).json({error: "The user could not be removed."})
+      })
+      res.status(200).json({user})
+    }
+  })
+  .catch(err => {
+    res.status(500).json({error: "The user could not be removed."})
+  })
+})
+
 server.listen(8000, () => console.log('API running on port 8000'));
